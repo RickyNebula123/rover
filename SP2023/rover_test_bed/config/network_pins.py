7e264d1086ce4a4688a7fd4561709ff698674074
ricardopb <nunya@tset.orgy>
2025-08-14 21:38:40 -0500

    added files to track

---
+++ SP2023/rover_test_bed/config/network_pins.py
@@ -0,0 +1,4 @@
+ROVER_IP_PIN          = '192.168.1.223'
+ROVER_PHONE_IP_PIN    = '172.20.10.6'
+ROVER_PORT_PIN        = 4422 # rover command server port
+ROVER_CAMERA_STREAM_PORT_PIN = 4040 # rover camera stream port
